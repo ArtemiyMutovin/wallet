e79a07e92130a535e71ae94e96c9ccff3deb62dd
Artemiy Mutovin <artemmutovin@mail.ru>
2022-08-23 14:31:10 -0400

    Database changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_094944) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_135207) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: ""
     t.string "first_name"
